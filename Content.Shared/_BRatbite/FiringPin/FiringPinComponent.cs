4aaf041708c01395c3573f1a81f604ca5f735340
iNV3RT3D <alextjorgensen@gmail.com>
2025-07-07 19:59:48 -0600

    Missing summary

--- Content.Shared/_BRatbite/FiringPin/FiringPinComponent.cs
+++ Content.Shared/_BRatbite/FiringPin/FiringPinComponent.cs
@@ -3,7 +3,7 @@ using Robust.Shared.GameStates;
 namespace Content.Shared._BRatbite.FiringPin;
 
 /// <summary>
-/// This is used for...
+/// This is used for telling FiringPinSystem to acknowledge this as a mindshield restricted weapon
 /// </summary>
 [RegisterComponent, NetworkedComponent]
 public sealed partial class FiringPinComponent : Component
