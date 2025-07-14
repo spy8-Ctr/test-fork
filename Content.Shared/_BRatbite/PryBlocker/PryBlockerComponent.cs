500104ba1c41bc607174c498a2220dcc286d5183
iNV3RT3D <alextjorgensen@gmail.com>
2025-07-13 19:19:47 -0600

    Component that blocks prying tiles underneath if object is anchored

---
+++ Content.Shared/_BRatbite/PryBlocker/PryBlockerComponent.cs
@@ -0,0 +1,10 @@
+namespace Content.Shared._BRatbite.PryBlocker;
+
+/// <summary>
+/// This is used for...
+/// </summary>
+[RegisterComponent]
+public sealed partial class PryBlockerComponent : Component
+{
+    
+}
