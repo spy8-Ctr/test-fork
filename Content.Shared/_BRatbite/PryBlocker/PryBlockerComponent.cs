8e93e8d9e3678f0cb0121c0c8cbc678ae72931d2
iNV3RT3D <alextjorgensen@gmail.com>
2025-07-20 08:23:32 -0600

    This is used for...

--- Content.Shared/_BRatbite/PryBlocker/PryBlockerComponent.cs
+++ Content.Shared/_BRatbite/PryBlocker/PryBlockerComponent.cs
@@ -1,7 +1,8 @@
 namespace Content.Shared._BRatbite.PryBlocker;
 
 /// <summary>
-/// This is used for...
+/// This is used for preventing tiles from being pried beneath an anchored entity with this component.
+/// Primarily for preventing lattice beneath field generators being cut.
 /// </summary>
 [RegisterComponent]
 public sealed partial class PryBlockerComponent : Component;
